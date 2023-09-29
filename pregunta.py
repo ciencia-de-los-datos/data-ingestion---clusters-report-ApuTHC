d344f225c71f1dc645b47a4f6a6e77d278a90527
Daniel Felipe Correa Zapata <83385274+ApuTHC@users.noreply.github.com>
2023-09-29 17:09:09 -0500

    Entrega

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,35 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 
 def ingest_data():
+    
+    dict_df = {
+        "cluster": [],
+        "cantidad_de_palabras_clave": [],
+        "porcentaje_de_palabras_clave": [],
+        "principales_palabras_clave": [],
+    }
 
-    #
-    # Inserte su código aquí
-    #
-
+    aux_row = ""
+    
+    with open('clusters_report.txt', 'r') as archivo:
+        for index, linea in enumerate(archivo):
+            if index >= 4:
+                # print(linea.strip())
+                if linea.strip() == "":
+                    columnas = re.sub(r'\s+', ' ', aux_row.strip())
+                    columnas = re.sub(r'\.', '', columnas)
+                    cols = columnas.split(" % ")
+                    col = cols[0].split(" ")
+                    dict_df["cluster"] += [int(col[0])]
+                    dict_df["cantidad_de_palabras_clave"] += [int(col[1])]
+                    dict_df["porcentaje_de_palabras_clave"] += [float(col[2].replace(",", "."))]
+                    dict_df["principales_palabras_clave"] += [cols[1]]
+                    aux_row = ""
+                else:
+                    aux_row += " " + linea.strip()
+    df = pd.DataFrame(dict_df)
     return df
